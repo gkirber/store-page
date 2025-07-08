87749a971e8f13a1318a7454d1802c9206df7c52
gkirber <george.kirber@gmail.com>
2025-07-08 23:22:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { products1 } from "./data1.js";
+import { products2 } from "./data2.js";
+
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const productsContainer = document.getElementById("products-container");
+const loader = document.getElementById("loader");
+
